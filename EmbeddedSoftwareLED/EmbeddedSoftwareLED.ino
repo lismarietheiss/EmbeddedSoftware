c02c2cf77e6182394bf1427ea17c5ee08f2f4900
LisaTheiss <lisathei@student.arteveldehs.be>
2021-10-26 21:04:18 +0200

    Update EmbeddedSoftwareLED.ino

--- EmbeddedSoftwareLED/EmbeddedSoftwareLED.ino
+++ EmbeddedSoftwareLED/EmbeddedSoftwareLED.ino
@@ -10,7 +10,6 @@ intervalTimer readInputTimer(50U);
 intervalTimer displayValuesTimer(1000U);
 
 void setup() {
-  //delay(2000U);
   pinMode(photoSensor,INPUT);
   pinMode( LED_Pin, OUTPUT );
   Serial.begin(9600);
@@ -21,13 +20,9 @@ void loop() {
   photoValue = analogRead(photoSensor);
   Serial.println(photoValue);
   LEDvalue = map(photoValue, 0, 200, 20, 0);
-  //int lightValue = constrain(LEDvalue, 0,255);//the contain output values within 0-255 range
-  //analogWrite(LED_Pin, lightValue);  
-  //delay (500);
   }
   if (writeOutputTimer.expired()){
   photoValue = constrain(LEDvalue, 0,255);//the contain output values within 0-255 range
   analogWrite(LED_Pin, photoValue);  
-  //delay (500);
   }
 }
